a7f6a04df531cbd36b38f5370d82ad5d33f80cea
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-12-17 12:55:52 +0300

    Navbar and footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flexBetween max-container  px-6 lg:px-20 3xl:px-0 relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" height={29} width={74}></Image>
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-black flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="bg-green-90 px-8 py-4 text-white transition-all hover:bg-black"
+        />
+      </div>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  );
 };
 
 export default Navbar;
