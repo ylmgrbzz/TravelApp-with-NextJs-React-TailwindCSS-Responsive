e6b6e1e4a226259da5fd330469818b8e92706a45
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-12-17 12:14:35 +0300

    structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <main className="relative overflow-hidden">{children}</main>
         <Footer />
       </body>
     </html>
